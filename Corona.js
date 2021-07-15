9fd00f7a44ab59ad2a00df2f14f53f2a13ba2c8e
corona45532 <83106035+corona45532@users.noreply.github.com>
2021-07-15 13:22:25 +0300

    Create Corona.js

---
+++ Corona.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const Yaxi = new Discord.Client();
+//////// playing or dnd  streaming ////////
+Yaxi.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `You only live once, so try to live for God.`,
+ 
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    corona.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+///////////////////////
+corona.on("ready", () => {
+    var join = corona.channels.get(""); /// id voice
+    if (join) join.join();
+  });
+//////////////// tokn lera da bne 
+corona.login("");
